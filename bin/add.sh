df4a47e21f0e5e28950272207f5842179a509a2e
Miguel Vieira <jmvieira@gmail.com>
2018-05-24 14:15:58 +0100

    Added options to choose which version of the script to use (dev, master), and option to add to xml files.

--- bin/add.sh
+++ bin/add.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sed -i 's/<\/body>/<script defer=\"defer\" src=\"https\:\/\/kdl.kcl.ac.uk\/sla-acpp\/js\/sla.js\" type=\"text\/javascript\"><\/script>\n<\/body>/' $1
+OPTIND=1
+
+URL="\"https\:\/\/kdl.kcl.ac.uk\/sla-acpp\/js\/sla.js\""
+XML=""
+
+while getopts "dx" opt; do
+    case "$opt" in
+        d)
+            URL="\"https\:\/\/rawgit.com\/kingsdigitallab\/sla-acpp\/develop\/js\/sla.js\""
+            ;;
+        x)
+            XML="\&#160;"
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+sed -i "s,<\/body>,<script defer=\"defer\" src=$URL type=\"text\/javascript\">$XML<\/script>\n<\/body>," $@
