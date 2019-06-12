837b3e6cf35ed5a3b8e641e91730b2367e67dde9
Miguel Vieira <jmvieira@gmail.com>
2019-06-12 10:44:39 +0100

    Code cleanup.

--- js/sla.js
+++ js/sla.js
@@ -1,140 +1,142 @@
 var body = document.getElementsByTagName('body')[0]
 
 body.onload = function() {
-    addCSS()
+  addCSS()
 
-    addSLA()
+  addSLA()
 
-    addCookieDisclaimer()
+  addCookieDisclaimer()
 
-    showCookieDisclaimer()
+  showCookieDisclaimer()
 }
 
 function addCSS() {
-    var link = document.createElement('link')
-    link.href = getBasePath() + '/../css/screen.css'
-    link.type = 'text/css'
-    link.rel = 'stylesheet'
-    link.media = 'screen, projection'
+  let link = document.createElement('link')
+  link.href = getBasePath() + '/../css/screen.css'
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.media = 'screen, projection'
 
-    document.getElementsByTagName('head')[0].appendChild(link)
+  document.getElementsByTagName('head')[0].appendChild(link)
 }
 
 function getBasePath() {
-    var scripts = document.getElementsByTagName('script')
-    var jsFile = scripts[scripts.length - 1].src.split('?')[0]
+  const jsFile = getJSFile().src.split('?')[0]
+  return jsFile.substr(0, jsFile.lastIndexOf('/'))
+}
 
-    return jsFile.substr(0, jsFile.lastIndexOf('/'))
+function getJSFile() {
+  const scripts = document.getElementsByTagName('script')
+
+  return scripts[scripts.length - 1]
 }
 
 function addSLA() {
-    var kdlUrl = 'https://www.kdl.kcl.ac.uk/'
+  const kdlUrl = 'https://www.kdl.kcl.ac.uk/'
 
-    var div = document.createElement('div')
-    div.className = 'sla'
+  let div = document.createElement('div')
+  div.className = 'sla'
 
-    var p = document.createElement('p')
-    p.innerHTML = 'This site is '
+  let p = document.createElement('p')
+  p.innerHTML = 'This site is '
 
-    var a = document.createElement('a')
-    a.href = kdlUrl + 'how-we-work/kdl-designed-developed-and-maintained/'
-    a.innerHTML = 'maintained'
+  let a = document.createElement('a')
+  a.href = kdlUrl + 'how-we-work/kdl-designed-developed-and-maintained/'
+  a.innerHTML = 'maintained'
 
-    p.appendChild(a)
-    p.appendChild(
-        document.createTextNode(' under a Service Level Agreement by ')
-    )
+  p.appendChild(a)
+  p.appendChild(document.createTextNode(' under a Service Level Agreement by '))
 
-    a = document.createElement('a')
-    a.href = kdlUrl
-    a.innerHTML = "King's Digital Lab"
+  a = document.createElement('a')
+  a.href = kdlUrl
+  a.innerHTML = "King's Digital Lab"
 
-    p.appendChild(a)
+  p.appendChild(a)
 
-    div.appendChild(p)
+  div.appendChild(p)
 
-    body.appendChild(div)
+  body.appendChild(div)
 }
 
 function addCookieDisclaimer() {
-    var div = document.createElement('div')
-    div.className = 'cookies-box hide'
-    div.id = 'cookie-disclaimer'
+  let div = document.createElement('div')
+  div.className = 'cookies-box hide'
+  div.id = 'cookie-disclaimer'
 
-    var p = document.createElement('p')
-    p.innerHTML = 'We use cookies to track usage and preferences.'
+  let p = document.createElement('p')
+  p.innerHTML = 'We use cookies to track usage and preferences.'
 
-    div.appendChild(p)
+  div.appendChild(p)
 
-    p = document.createElement('p')
+  p = document.createElement('p')
 
-    var a = document.createElement('a')
-    a.className = 'button open'
-    a.onclick = function() {
-        document.getElementById('privacy-policy').classList.toggle('hide')
-        document.getElementById('cookie-disclaimer').classList.toggle('large')
+  let a = document.createElement('a')
+  a.className = 'button open'
+  a.onclick = function() {
+    document.getElementById('privacy-policy').classList.toggle('hide')
+    document.getElementById('cookie-disclaimer').classList.toggle('large')
 
-        this.classList.toggle('open')
-        this.classList.toggle('close')
-    }
-    a.href = '#'
-    a.id = 'show-privacy-policy'
-    a.innerHTML = 'Privacy &amp; Cookie Policy'
+    this.classList.toggle('open')
+    this.classList.toggle('close')
+  }
+  a.href = '#'
+  a.id = 'show-privacy-policy'
+  a.innerHTML = 'Privacy &amp; Cookie Policy'
 
-    p.appendChild(a)
+  p.appendChild(a)
 
-    div.appendChild(p)
+  div.appendChild(p)
 
-    p = document.createElement('p')
+  p = document.createElement('p')
 
-    var button = document.createElement('button')
-    button.className = 'button'
-    button.innerHTML = 'I Accept'
-    button.id = 'close-cookie-disclaimer'
-    button.type = 'button'
+  let button = document.createElement('button')
+  button.className = 'button'
+  button.innerHTML = 'I Accept'
+  button.id = 'close-cookie-disclaimer'
+  button.type = 'button'
 
-    p.appendChild(button)
+  p.appendChild(button)
 
-    div.appendChild(p)
+  div.appendChild(p)
 
-    var iframe = document.createElement('iframe')
-    iframe.className = 'hide'
-    iframe.id = 'privacy-policy'
-    iframe.src = getBasePath() + '/../html/privacy-policy.html'
+  let iframe = document.createElement('iframe')
+  iframe.className = 'hide'
+  iframe.id = 'privacy-policy'
+  iframe.src = getBasePath() + '/../html/privacy-policy.html'
 
-    div.append(iframe)
+  div.append(iframe)
 
-    body.appendChild(div)
+  body.appendChild(div)
 }
 
 function showCookieDisclaimer() {
-    var key = 'cookie-disclaimer'
-    var value = 'cookie-disclaimer-set'
+  const key = 'cookie-disclaimer'
+  const value = 'cookie-disclaimer-set'
 
-    var disclaimer = document.getElementById('cookie-disclaimer')
+  const disclaimer = document.getElementById('cookie-disclaimer')
 
-    var cookieDisclaimerAccepted = getStorageItem(key)
+  const cookieDisclaimerAccepted = getStorageItem(key)
 
-    if (!cookieDisclaimerAccepted) {
-        disclaimer.className = disclaimer.className.replace(/ hide/, '')
-    }
+  if (!cookieDisclaimerAccepted) {
+    disclaimer.className = disclaimer.className.replace(/ hide/, '')
+  }
 
-    document.getElementById('close-cookie-disclaimer').onclick = function() {
-        setStorageItem(key, value)
-        disclaimer.className += ' hide'
-    }
+  document.getElementById('close-cookie-disclaimer').onclick = function() {
+    setStorageItem(key, value)
+    disclaimer.className += ' hide'
+  }
 }
 
 function getStorageItem(key) {
-    if (typeof Storage !== 'undefined') {
-        return localStorage.getItem(key)
-    }
+  if (typeof Storage !== 'undefined') {
+    return localStorage.getItem(key)
+  }
 
-    return null
+  return null
 }
 
 function setStorageItem(key, value) {
-    if (typeof Storage !== 'undefined') {
-        localStorage.setItem(key, value)
-    }
+  if (typeof Storage !== 'undefined') {
+    localStorage.setItem(key, value)
+  }
 }
