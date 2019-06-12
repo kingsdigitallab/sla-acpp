6a70716f7c9ed01866dd8577cfcff6788fae5c47
Miguel Vieira <jmvieira@gmail.com>
2019-06-12 11:21:24 +0100

    Update script and instructions.

--- bin/add.sh
+++ bin/add.sh
@@ -2,13 +2,17 @@
 
 OPTIND=1
 
-URL="\"https\:\/\/kdl.kcl.ac.uk\/sla-acpp\/js\/sla.js\""
+URL="https\:\/\/kdl.kcl.ac.uk\/sla-acpp\/js"
+SCRIPT="sla.js"
 XML=""
 
-while getopts "dx" opt; do
+while getopts "dsx" opt; do
     case "$opt" in
         d)
-            URL="\"https\:\/\/rawgit.com\/kingsdigitallab\/sla-acpp\/develop\/js\/sla.js\""
+            URL="https\:\/\/rawgit.com\/kingsdigitallab\/sla-acpp\/develop\/js"
+            ;;
+        s)
+            SCRIPT="static.js"
             ;;
         x)
             XML="\&#160;"
@@ -16,6 +20,6 @@ while getopts "dx" opt; do
     esac
 done
 
-shift $((OPTIND-1))
+URL="\"$URL\/$SCRIPT\""
 
 sed -i "s,<\/body>,<script defer=\"defer\" src=$URL type=\"text\/javascript\">$XML<\/script>\n<\/body>," $@
